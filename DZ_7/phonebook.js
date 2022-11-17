abf1e07d78434f1cf14f01369f51565060846a1b
K_A_A <banderas91@mail.ru>
2022-11-17 19:56:35 +0300

    Home_work_7

---
+++ DZ_7/phonebook.js
@@ -0,0 +1,4 @@
+[
+{"surname": "Васильев", "name": "Андрей", "telephone": "21345е343", "description": "йййййй"},
+{"surname": "Костин", "name": "Сергей", "telephone": "1111111111", "description": "кто-то"}
+]
